44b378d3a1b88519ea2fb9f1b73f92736d9faa98
GiKerche <gigikerche@gmail.com>
2022-01-15 15:44:06 -0300

    Adição da função de frase motivacional

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 import logging
-
 from telegram_config import dispatcher, updater
 from handlers.start_handler import start_handler
 from handlers.currency_handler import currency_handler
-
+from handlers.phrase_handler import phrase_handler
 
 def main():
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(currency_handler)
-
+    dispatcher.add_handler(phrase_handler)
     updater.start_polling()
 
 
